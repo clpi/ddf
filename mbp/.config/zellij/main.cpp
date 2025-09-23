33d4e61dbbe5d8ce8c16605af9ffc509caa8ec2f
Chris Pecunies <clp@clp.is>
2025-09-22 19:45:54 -0700

    adding mbp

---
+++ mbp/.config/zellij/main.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int const argc, char *const argv[], char *const envp[]) {
+  for (int i = 0; i < argc; i++) {
+  }
+}
+;j
