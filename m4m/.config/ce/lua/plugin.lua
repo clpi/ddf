52428b0eacdf37a30dae0390c330a90af43e3944
Chris Pecunies <chris@pecunies.com>
2025-09-29 18:24:05 -0700

    adding m4m

--- m4m/.config/ce/lua/plugin.lua
+++ m4m/.config/ce/lua/plugin.lua
@@ -1,8 +1,93 @@
-
 local plugins = {
-  {'romgrk/barbar.nvim',
+  { "mfussenegger/nvim-dap", config = function()
+  end},
+  {
+    "xzbdmw/colorful-menu.nvim",
+    enabled = true,
     dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      { "onsails/lspkind.nvim", opts = {} },
+    },
+    opts = {
+      ls = {
+        lua_ls = {
+          -- Maybe you want to dim arguments a bit.
+          arguments_hl = "@comment",
+        },
+        gopls = {
+          align_type_to_right = true,
+          add_colon_before_type = false,
+          preserve_type_when_truncate = true,
+        },
+        ts_ls = {
+          extra_info_hl = "@comment",
+        },
+        vtsls = {
+          -- false means do not include any extra info,
+          -- see https://github.com/xzbdmw/colorful-menu.nvim/issues/42
+          extra_info_hl = "@comment",
+        },
+        ["rust-analyzer"] = {
+          -- Such as (as Iterator), (use std::io).
+          extra_info_hl = "@comment",
+          -- Similar to the same setting of gopls.
+          align_type_to_right = true,
+          -- See https://github.com/xzbdmw/colorful-menu.nvim/pull/36
+          preserve_type_when_truncate = true,
+        },
+        clangd = {
+          -- Such as "From <stdio.h>".
+          extra_info_hl = "@comment",
+          -- Similar to the same setting of gopls.
+          align_type_to_right = true,
+          -- the hl group of leading dot of "•std::filesystem::permissions(..)"
+          import_dot_hl = "@comment",
+          -- See https://github.com/xzbdmw/colorful-menu.nvim/pull/36
+          preserve_type_when_truncate = true,
+        },
+        zls = {
+          -- Similar to the same setting of gopls.
+          align_type_to_right = true,
+        },
+        roslyn = {
+          extra_info_hl = "@comment",
+        },
+        dartls = {
+          extra_info_hl = "@comment",
+        },
+        -- The same applies to pyright/pylance
+        basedpyright = {
+          -- It is usually import path such as "os"
+          extra_info_hl = "@comment",
+        },
+        pylsp = {
+          extra_info_hl = "@comment",
+          -- Dim the function argument area, which is the main
+          -- difference with pyright.
+          arguments_hl = "@comment",
+        },
+        -- If true, try to highlight "not supported" languages.
+        fallback = true,
+        -- this will be applied to label description for unsupport languages
+        fallback_extra_info_hl = "@comment",
+      },
+      -- If the built-in logic fails to find a suitable highlight group for a label,
+      -- this highlight is applied to the label.
+      fallback_highlight = "@variable",
+      -- If provided, the plugin truncates the final displayed text to
+      -- this width (measured in display cells). Any highlights that extend
+      -- beyond the truncation point are ignored. When set to a float
+      -- between 0 and 1, it'll be treated as percentage of the width of
+      -- the window: math.floor(max_width * vim.api.nvim_win_get_width(0))
+      -- Default 60.
+      max_width = 60,
+    },
+  },
+  { "stevearc/overseer.nvim", opts = {} },
+  { "stevearc/dressing.nvim", opts = {} },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
     init = function() vim.g.barbar_auto_setup = false end,
@@ -15,194 +100,197 @@ local plugins = {
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
   {
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = { 'rafamadriz/friendly-snippets' },
 
-  -- use a release tag to download pre-built binaries
-  version = '1.*',
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
+    -- use a release tag to download pre-built binaries
+    version = '1.*',
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = 'default' },
 
-    appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = { documentation = { auto_show = false } },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" }
     },
-
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    opts_extend = { "sources.default" }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "Saghen/blink.cmp"
     },
-
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
-},
-  {"neovim/nvim-lspconfig", 
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "Saghen/blink.cmp"
-  },
-  config=function()
-    require("mason").setup()
-    require("mason-lspconfig").setup{
-      ensure_installed = {},
-    }
-    local lspconfig = require("lspconfig")
-    local on_attach = function(client, bufnr)
-      local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-      buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-      local opts = { noremap=true, silent=true }
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e', '<Cmd>lua vim.diagnostic.open_float()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<Cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<Cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>q', '<Cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup {
+        ensure_installed = {},
+      }
+      local lspconfig = require("lspconfig")
+      local on_attach = function(client, bufnr)
+        local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+        buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+        local opts = { noremap = true, silent = true }
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl',
+          '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e', '<Cmd>lua vim.diagnostic.open_float()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<Cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<Cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>q', '<Cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+      end
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+      local servers = { 'lua_ls', 'tsserver', 'rust_analyzer', 'pyright', 'gopls' }
+      for _, lsp in ipairs(servers) do
+        vim.lsp.config(lsp, {
+          on_attach = on_attach,
+          capabilities = capabilities,
+        })
+      end
     end
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
-    local servers = { 'lua_ls', 'tsserver', 'rust_analyzer', 'pyright', 'gopls' }
-    for _, lsp in ipairs(servers) do
-      vim.lsp.config(lsp, {
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-    end
-  end},
+  },
 
-	{
-		"mason-org/mason.nvim",
-		opts = {
-			install_root_dir = "/Users/clp/.local/share/nvim/mason",
-		},
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-			},
-			automatic_enable = true,
-		},
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
-	},
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      install_root_dir = "/Users/clp/.local/share/nvim/mason",
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+      },
+      automatic_enable = true,
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  },
 
-	{
-		"SmiteshP/nvim-navic",
-		dependencies = { "neovim/nvim-lspconfig" },
-	},
-	{
-		"akinsho/toggleterm.nvim",
-		opts = { size = 20, autochdir = true, open_mapping = [[<C-O>]], direction = "horizontal" },
-	},
-	{
-		"folke/neoconf.nvim",
-		opts = {},
-	},
-	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
-	{
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-			},
-		},
-	},
-  {"nvim-lualine/lualine.nvim", opts = {}},
-  {"nvim-tree/nvim-tree.lua",opts={}},
-  {"nvim-tree/nvim-web-devicons"},
-  {"nvim-telescope/telescope.nvim",opts={}},
-  {"nvim-telescope/telescope-fzf-native.nvim",build="make"},
-  {"folke/which-key.nvim"},
-  {"lewis6991/gitsigns.nvim",opts={}},
+  {
+    "SmiteshP/nvim-navic",
+    dependencies = { "neovim/nvim-lspconfig" },
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    opts = { size = 20, autochdir = true, open_mapping = [[<C-O>]], direction = "horizontal" },
+  },
+  {
+    "folke/neoconf.nvim",
+    opts = {},
+  },
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    },
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { "nvim-lualine/lualine.nvim",                opts = {} },
+  { "nvim-tree/nvim-tree.lua",                  opts = {} },
+  { "nvim-tree/nvim-web-devicons" },
+  { "nvim-telescope/telescope.nvim",            opts = {} },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  { "folke/which-key.nvim" },
+  { "lewis6991/gitsigns.nvim",                  opts = {} },
   -- {"numToStr/Comment.nvim"},
-  {"windwp/nvim-autopairs", opts={}},
-  {"windwp/nvim-ts-autotag", opts={}},
-  {"akinsho/bufferline.nvim",version="v3.*",dependencies={"nvim-tree/nvim-web-devicons"}},
+  { "windwp/nvim-autopairs",                    opts = {} },
+  { "windwp/nvim-ts-autotag",                   opts = {} },
+  { "akinsho/bufferline.nvim",                  version = "v3.*", dependencies = { "nvim-tree/nvim-web-devicons" } },
   -- {"lukas-reineke/indent-blankline.nvim"},
   -- {"goolord/alpha-nvim"},
   -- {"folke/tokyonight.nvim"},
   -- {"catppuccin/nvim",name="catppuccin"},
-  {"rebelot/kanagawa.nvim"},
+  { "rebelot/kanagawa.nvim" },
   -- {"EdenEast/nightfox.nvim"},
-  {"nvim-mini/mini.animate",version=false},
-  {"nvim-mini/mini.cursorword",version=false},
-  {"nvim-mini/mini.hipatterns",version=false},
-  {"nvim-mini/mini.clue",version=false,opts={}},
-  {"nvim-mini/mini.indentscope",version=false},
-  {"nvim-mini/mini.icons",version=false},
-  {"nvim-mini/mini.bufremove",version=false},
-	{
-		"Bekaboo/dropbar.nvim",
-		-- optional, but required for fuzzy finder support
-		dependencies = {
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-		},
-		config = function()
-			local dropbar_api = require("dropbar.api")
-			vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
-			vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-			vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
-		end,
-	},
+  { "nvim-mini/mini.animate",                   version = false },
+  { "nvim-mini/mini.cursorword",                version = false },
+  { "nvim-mini/mini.hipatterns",                version = false },
+  { "nvim-mini/mini.clue",                      version = false, opts = {} },
+  { "nvim-mini/mini.indentscope",               version = false },
+  { "nvim-mini/mini.icons",                     version = false },
+  { "nvim-mini/mini.bufremove",                 version = false },
+  {
+    "Bekaboo/dropbar.nvim",
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+    },
+    config = function()
+      local dropbar_api = require("dropbar.api")
+      vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+      vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+      vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+    end,
+  },
   { "github/copilot.vim" },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -210,8 +298,8 @@ local plugins = {
     lazy = false,
   },
 }
-require("lazy").setup{
-	install = { colorscheme = { "habamax" } },
-	checker = { enabled = true },
-	spec = plugins,
+require("lazy").setup {
+  install = { colorscheme = { "habamax" } },
+  checker = { enabled = true },
+  spec = plugins,
 }
